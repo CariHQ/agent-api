42fd2995f62713e5f31ff6691aa861bdab7ec6f4
Kenroy George <kenroy@garakami.com>
2020-07-04 01:25:34 +0100

    feat(auth): add rate limiter as middleware

--- middleware/index.js
+++ middleware/index.js
@@ -1,13 +1,26 @@
 const cors = require('cors');
 const bodyParser = require('body-parser');
+const helmet = require('helmet'); // Helmet
+const mongoSanitize = require('express-mongo-sanitize'); // Data Sanitization against NoSQL Injection Attacks
+const xss = require('xss-clean'); // Data Sanitization against XSS attacks
 
 const logMiddleware = require('../log').middleware;
 const validation = require('./validate');
+const limiter = require('./ratelimiter');
 const notFound = require('./404');
 const { resultHandler, errorHandler } = require('./result');
 
 module.exports = {
-    before: [cors(), logMiddleware, bodyParser.json(), validation],
+    before: [
+        cors(),
+        helmet(),
+        logMiddleware,
+        bodyParser.json({ limit: '10kb' }),
+        limiter,
+        validation,
+        mongoSanitize(),
+        xss()
+    ],
 
     after: [notFound, resultHandler, errorHandler]
 };
