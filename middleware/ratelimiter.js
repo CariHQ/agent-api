79620c3cd7e9d8c0cf9c42d40ab00bdffa758ce4
Kenroy George <kenroy@garakami.com>
2020-07-04 13:57:36 +0100

    chore(*): update rate limit to 500 calls per hour

--- middleware/ratelimiter.js
+++ middleware/ratelimiter.js
@@ -1,7 +1,7 @@
 const rateLimit = require('express-rate-limit');
 
 const limit = rateLimit({
-    max: 100, // max requests
+    max: 500, // max requests
     windowMs: 60 * 60 * 1000, // 1 Hour
     message: 'Too many requests' // message to send
 });
